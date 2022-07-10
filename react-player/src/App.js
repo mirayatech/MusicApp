bdd408f5ee209566b5f1e465359a390b77ab6f94
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-10 18:55:16 +0200

    initial styling

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+// Importning style
+import "./styles/app.scss";
+
+// Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
       <Song />
       <Player />
     </div>
